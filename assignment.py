85e5b8e7e91ecdeb24aff761ee2db6730249acdd
jeremyc009 <jeremyc009@deltalearns.ca>
2020-10-20 11:31:16 -0700

    s

--- assignment.py
+++ assignment.py
@@ -1,7 +1,3 @@
-import pyautogui
+import pyautogui as p
 
-<<<<<<< HEAD
-pyautogui.mouseinfo()
-=======
 p.mouseInfo()
->>>>>>> 0e857b23101c91031797119f64fc6df5c31ceb64
---
+++ planning.py
@@ -0,0 +1,7 @@
+"""
+This is the list of actions my code needs to do.
+1: Start on the Start screen (The 6 squares with the different game modes)
+2: Choose the arcade mode.
+3: Choose normal mode.
+4: Click the black square with the arrow cut out of it. 
+5: Click the black squares
